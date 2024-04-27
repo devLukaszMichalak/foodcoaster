f22e6bdfa231fb3f1abb65aa4c103f7d4caf325a
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2024-04-27 22:48:11 +0200

    example recipes

--- src/app/card-stack/data/recipe/recipe.service.ts
+++ src/app/card-stack/data/recipe/recipe.service.ts
@@ -1,28 +1,13 @@
 import { computed, Injectable, Signal, signal } from '@angular/core';
+import arrayShuffle from 'array-shuffle';
+import { recipes } from './recipes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
   
-  private _recipes = signal<Recipe[]>([
-    {
-      imgUrl: 'https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340',
-      name: 'test'
-    },
-    {
-      imgUrl: 'https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340',
-      name: 'test2'
-    },
-    {
-      imgUrl: 'https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340',
-      name: 'test3'
-    },
-    {
-      imgUrl: 'https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340',
-      name: 'test4'
-    }
-  ]);
+  private _recipes = signal<Recipe[]>(arrayShuffle(recipes));
   
   get recipes(): Signal<Recipe[]> {
     return computed(() => this._recipes());
---
+++ src/app/card-stack/data/recipe/recipes.ts
@@ -0,0 +1,20 @@
+import { Recipe } from './recipe.service';
+
+export const recipes: Recipe[] = [
+  {
+    imgUrl: 'assets/images/439881546_812546737439401_7179664926974566_n.jpg',
+    name: 'Sushi'
+  },
+  {
+    imgUrl: 'assets/images/439762954_835935231671136_6328973457610153309_n.jpg',
+    name: 'Curry'
+  },
+  {
+    imgUrl: 'assets/images/439273274_802815137948777_5345856065112772872_n.jpg',
+    name: 'Ryż kimchi'
+  },
+  {
+    imgUrl: 'assets/images/439914843_829133185908479_8509747142550053271_n.jpg',
+    name: 'Warzywa azjatyckie'
+  }
+];
